f19235ba91a5dadbfd108cd587684302676ff6f6
WinterYukky <WinterYukky@gmail.com>
2022-05-30 23:11:35 +0000

    feat: main route table

--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,54 @@
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { IRouteTable, IVpc } from "aws-cdk-lib/aws-ec2";
+import {
+  AwsCustomResource,
+  AwsCustomResourcePolicy,
+  AwsSdkCall,
+  PhysicalResourceId,
+} from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
 
-export interface AwsCdkMainRouteTableProps {
-  // Define construct properties here
-}
-
-export class AwsCdkMainRouteTable extends Construct {
-
-  constructor(scope: Construct, id: string, props: AwsCdkMainRouteTableProps = {}) {
+/**
+ * Main route table.
+ */
+export class MainRouteTable extends Construct {
+  private constructor(scope: Construct, id: string) {
     super(scope, id);
+  }
 
-    // Define construct contents here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkMainRouteTableQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  /**
+   * Import the main route table from VPC.
+   */
+  public static fromVpc(scope: Construct, id: string, vpc: IVpc): IRouteTable {
+    const describeRouteTables: AwsSdkCall = {
+      service: "EC2",
+      action: "describeRouteTables",
+      parameters: {
+        Filters: [
+          {
+            Name: "association.main",
+            Values: ["true"],
+          },
+          {
+            Name: "vpc-id",
+            Values: [vpc.vpcId],
+          },
+        ],
+      },
+      physicalResourceId: PhysicalResourceId.of(vpc.vpcId),
+    };
+    const describeRouteTablesResult = new AwsCustomResource(scope, id, {
+      resourceType: "Custom::DescribeRouteTables",
+      policy: AwsCustomResourcePolicy.fromSdkCalls({
+        resources: ["*"],
+      }),
+      installLatestAwsSdk: false,
+      onCreate: describeRouteTables,
+      onUpdate: describeRouteTables,
+    });
+    return {
+      routeTableId: describeRouteTablesResult.getResponseField(
+        "RouteTables.0.RouteTableId"
+      ),
+    };
   }
 }
